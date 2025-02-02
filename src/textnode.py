940a1e9b73351eedf56c079adeb393f34ad72949
RaulCD3597 <raulcd3597@gmail.com>
2025-02-02 09:09:51 -0400

    textnode to html add

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from typing import Optional
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -30,3 +32,19 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node:TextNode) -> LeafNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href": str(text_node.url)})
+        case TextType.IMAGE:
+            return LeafNode("img", "", props={"src": str(text_node.url), "alt": text_node.text})
