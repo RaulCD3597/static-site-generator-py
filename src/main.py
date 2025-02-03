acf568b87cbca4c8be61c74fddb45cbc2bcab17b
RaulCD3597 <raulcd3597@gmail.com>
2025-02-03 09:02:44 -0400

    add copy forder function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
 """SSG entrypoint"""
 
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    tn = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(tn)
+    cp_directory("static", "public")
+
+
+def cp_directory(src: str, dst: str) -> None:
+    if not os.path.exists(src):
+        raise ValueError("source directory does not exists")
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+    content = os.listdir(src)
+    for item in content:
+        item_path = os.path.join(src, item)
+        item_dst_path = os.path.join(dst, item)
+        if os.path.isfile(item_path):
+            shutil.copy2(item_path, item_dst_path)
+        else:
+            cp_directory(item_path, item_dst_path)
 
 
 if __name__ == "__main__":
